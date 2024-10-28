0340922f71ab9c53dc171ac0b993554795be405b
Henning Prien <henning369@gmx.de>
2024-10-28 12:14:30 +0100

    added exercised 1-3

--- main.cpp
+++ main.cpp
@@ -3,13 +3,49 @@
 // imported from iostream
 using namespace std;
 
-int exercise_1() {
+// install qt
+void exercise_1() {
     cout << "Hello World!" << endl;
+}
+
+// debugging
+void exercise_2() {
+    float result;
+    int number1 = 7;
+    int number2 = 8;
+    result = number1 / number2; // 0,875 -> 0
+    cout << result;
+}
+
+// finding errors
+// documenting functionality
+void exercise_3() {
+    // just print to console
+    cout << "los" << " geht's!" << endl;
+
+    int ii = 0;
+    int sum = 0;
+
+    // loop from 0 to 9
+    while (ii <= 9) {
+        sum = sum + ii;
+        ii = ii + 1;
+    }
+
+    // print sum of numbers 0-9
+    cout << "Ergebnis: " << sum << endl;
+}
+
+void exercise_4() {
+
 }
 
 int main() {
     // call exercise
-    exercise_1();
+    //exercise_1();
+    //exercise_2();
+    //exercise_3();
+    exercise_4();
 
     // return 0 to system
     return 0;
