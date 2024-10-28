ecf0fba7929e448f850c3a47cb313990d1d07361
Henning Prien <henning369@gmx.de>
2024-10-28 11:56:33 +0100

    initial setup

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// imported from iostream
+using namespace std;
+
+int exercise_1() {
+    cout << "Hello World!" << endl;
+}
+
+int main() {
+    // call exercise
+    exercise_1();
+
+    // return 0 to system
+    return 0;
+}
